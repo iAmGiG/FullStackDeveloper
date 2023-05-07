adae189498d90eba25cb3a8417b6d5dbf10f149b
fabrezi <fkschon@gmail.com>
2023-05-07 07:46:50 -0400

    make the link pop-up

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const port = 4200;
 
 const app = express();
 
-const PORT = 4200;
-app.listen(PORT, () =>{
-    console.log(`Server is running at port: ${PORT}`);
+
+app.listen( port, () =>{
+    console.log(`Server is running at link ==> http://localhost:${port}`);
 })
