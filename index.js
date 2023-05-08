5c20b2c7827a0d842266b3787ca312d5b1b3b794
fabrezi <fkschon@gmail.com>
2023-05-08 10:48:46 -0400

    update(server): add path

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const emoji = require('node-emoji');
 const port = 4200;
+const path = require('path');
 
 const app = express();
 
@@ -9,4 +10,8 @@ const air = emoji.get('airplane');
 
 app.listen( port, () =>{
     console.log(`Server is running at link ==> http://localhost:${port}` , air);
+})
+
+app.get('/' , (req,res) => {
+    res.sendFile(path.join(__dirname, 'src/front.html'));
 })
