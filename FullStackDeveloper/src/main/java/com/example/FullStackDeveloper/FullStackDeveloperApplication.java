90b22e6151d0066b53eabde739594ab87310c886
fabrezi <fkschon@gmail.com>
2023-05-22 15:16:58 -0400

    chore(pom): update to java 11

--- FullStackDeveloper/src/main/java/com/example/FullStackDeveloper/FullStackDeveloperApplication.java
+++ FullStackDeveloper/src/main/java/com/example/FullStackDeveloper/FullStackDeveloperApplication.java
@@ -2,12 +2,22 @@ package com.example.FullStackDeveloper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import  org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class FullStackDeveloperApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FullStackDeveloperApplication.class, args);
 	}
+
+	@GetMapping("/hola")
+	public String hola(@RequestParam(value = "name", defaultValue = "world") String name) {
+		return String.format("Hi %s!" , name);
+	}
+
 
 }
