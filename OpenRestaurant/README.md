ee48a7c1e350de06cc545d8236262b03a348cc81
Chris R <gigachris1@hotmail.com>
2023-05-23 20:08:24 -0400

    Getting the feet wet with understanding typeScript Learning about the tools and the instruments of the framework.

--- OpenRestaurant/README.md
+++ OpenRestaurant/README.md
@@ -5,6 +5,7 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+* updated scripts to include `start: ng serve --open` to auto open when using `npm start` to run. Performs same operation as `ng serve` with the inclusion to auto open the (default) browser on complete.
 
 ## Code scaffolding
 
--- OpenRestaurant/src/app/app.component.ts
+++ OpenRestaurant/src/app/app.component.ts
@@ -2,10 +2,26 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 // import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 // import { AntiHero } from '../../models/anti-hero.interface';
 
+type Restaurant = 'Texas Mex-as' | 'Boston' | 'New York' | 'Philly';
+enum RestaurantCoreFood {
+  bigTexasSteak,
+  bigSalad,
+  bigPhilly,
+  bigBostonCreamPie,
+  bigNewYorkPizza,
+}
+enum RestaurantPricing {
+  water = 0,
+  bigTexasSteak = 5,
+  bigSalad = 3,
+  bigPhilly = 4,
+  bigBostonCreamPie = 2,
+  bigNewYorkPizza = 1.99,
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent implements OnInit {
   @Input() selectedId = '';
@@ -13,17 +29,20 @@ export class AppComponent implements OnInit {
   // @Output() action = new EventEmitter();
   // form: FormGroup;
   title = 'OpenRestaurant';
-  restaurantName: string = 'Texas Mexas';
+  myCoreFood: RestaurantCoreFood = RestaurantCoreFood.bigTexasSteak;
+  myPricing: RestaurantPricing = RestaurantPricing.bigTexasSteak;
+  restaurantType: Restaurant = 'Texas Mex-as';
+  restaurantName: string = 'Texas Mex-as';
   restaurantCapacity: number = 142;
   currentOccupancy: number = 65;
-  currentOccupancyPercentaage: number = (this.currentOccupancy / this.restaurantCapacity) * 100
-  isFullString: string = "Not Yet";
+  currentOccupancyPercentage: number = Math.abs(
+    Math.round((this.currentOccupancy / this.restaurantCapacity) * 100)
+  );
+  isFullString: string = 'Not Yet';
 
-  constructor() {
-    
-  }
+  constructor() {}
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.checkCapacity();
   }
 
@@ -32,5 +51,16 @@ export class AppComponent implements OnInit {
       this.isFullString = "We've hit capacity";
     }
   }
+}
 
+// export
+function RestaurantPricingCalculator(
+  RestaurantPricing1: any,
+  RestaurantPricing2: any,
+  tax: any,
+  // ..._fees: any,
+  tip?: any
+): any {
+  // throw new Error('Function not implemented.');
+  return RestaurantPricing1 + RestaurantPricing2 + tax + tip;
 }
